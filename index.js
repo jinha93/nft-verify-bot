f2d753b9742064f1350802a760c7bd954f429123
ParkJinha <jhhhhh93@naver.com>
2022-08-30 18:42:43 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const { request } = require('undici');
+const express = require('express');
+const { clientId, clientSecret, port } = require('./config.json');
+
+async function getJSONResponse(body) {
+	let fullBody = '';
+
+	for await (const data of body) {
+		fullBody += data.toString();
+	}
+	return JSON.parse(fullBody);
+}
+
+const app = express();
+
+app.get('/', async ({ query }, response) => {
+	const { code } = query;
+	let id = "";
+
+	if (code) {
+		try {
+			const tokenResponseData = await request('https://discord.com/api/oauth2/token', {
+				method: 'POST',
+				body: new URLSearchParams({
+					client_id: clientId,
+					client_secret: clientSecret,
+					code,
+					grant_type: 'authorization_code',
+					redirect_uri: `http://localhost:${port}`,
+					scope: 'identify',
+				}).toString(),
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			});
+
+			const oauthData = await getJSONResponse(tokenResponseData.body);
+			console.log(oauthData);
+
+			const userResult = await request('https://discord.com/api/users/@me', {
+				headers: {
+					authorization: `${oauthData.token_type} ${oauthData.access_token}`,
+				},
+			});
+			const userData = await getJSONResponse(userResult.body);
+			console.log(userData);
+			console.log(userData.id);
+			id = userData.id;
+			response.set("id",id);
+
+		} catch (error) {
+			// NOTE: An unauthorized token will not throw an error
+			// tokenResponseData.statusCode will be 401
+			console.error(error);
+		}
+	}
+	return response.sendFile('index.html', { root: '.' });
+});
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
